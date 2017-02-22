b4a5a01f0f9f466f73222e754a6ed516e2704ec1
meloMacPro <meloMacPro@crossmacdeMacBook-Pro.local>
2017-02-22 12:33:19 +0800

    tornado

---
+++ code/0023/main.py
@@ -0,0 +1,50 @@
+#main.py
+import os
+import time
+import tornado.ioloop
+import tornado.web
+import tornado.gen
+from tornado.concurrent import Future
+from tornado.httpclient import AsyncHTTPClient
+from tornado.options import define, options, parse_command_line
+
+define("debug", default=True, help="run in debug mode")
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        return self.render('index.html', messages=['1', '2', '3', 'response'])
+
+
+k = 0
+class MessageHandler(tornado.web.RequestHandler):
+    def post(self):
+        return self.write({'msg':['ok'], 'len': 1})
+    @tornado.gen.coroutine
+    def get(self):
+        global k
+        self.future = Future()
+        k += 1
+        self.future.set_result({'msg': k})
+        messages = yield self.future
+        if self.request.connection.stream.closed():
+            return
+        self.write(messages)
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/message", MessageHandler)
+        ],
+        cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        xsrf_cookies=True,
+        debug=options.debug,
+    )
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    print('>> running: 8888')
+    tornado.ioloop.IOLoop.current().start()
