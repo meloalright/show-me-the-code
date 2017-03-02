b9ab45af651a9048948e9db99ed21f4199322f4b
meloMacPro <meloMacPro@crossmacdeMacBook-Pro.local>
2017-03-03 00:05:51 +0800

    0024

---
+++ code/0024/my-project/src/components/Hello.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="hello">
+    <div class="input-line">
+      <input placeholder="name" class="input-cell"></input>
+    </div>
+    <div class="input-line">
+      <input placeholder="password" type="password" class="input-cell"></input>
+    </div>
+    <div class="mt30">
+      <button class="btn-cell blue-btn">LOGIN</button>
+    </div>
+    <div class="btn-line">
+      <button class="btn-cell light-btn">NO ACOUNT</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input,button,select,textarea {
+  outline:none
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.input-line {
+  margin: 10px 0;
+}
+.input-cell {
+  width: 250px;
+  font-size: 12px;
+  line-height: 22px;
+  text-indent: 5px;
+}
+
+
+
+.mt30 {
+  margin-top: 30px;
+}
+.btn-line {
+  margin: 10px 0 0 0;
+}
+.btn-cell {
+  cursor: pointer;
+  width: 200px;
+  font-size: 12px;
+  line-height: 22px;
+  transition: 0.4s width ease-in-out;
+}
+
+.btn-cell:hover {
+  cursor: pointer;
+  width: 220px;
+  font-size: 12px;
+  line-height: 22px;
+}
+
+.blue-btn {
+  border: solid 1px #38a6ec;
+  background-color: #38a6ec;
+  color: #FFF;
+}
+.light-btn {
+  border: solid 1px #38a6ec;
+  background-color: #FFF;
+  color: #38a6ec;
+}
+</style>
